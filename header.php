f08b7ee1158f60cdf1a125462ea662c2e578cb41
Rodrigo Cruz <rodrigonascimentocruz@gmail.com>
2022-11-28 20:26:25 -0300

    menu ajustado-andamento

--- header.php
+++ header.php
@@ -10,31 +10,70 @@
 </head>
 <body <?php body_class(); ?>>
     <header>
-        <section>
+        <section class="menu-mobile">
             <nav class="nav">
-                <div  class="d-flex justify-content-left align-items-center container">
-                    <div>
-                    <?php if(has_custom_logo() ): ?>
-                        <?php the_custom_logo(); ?>
-                    <?php else: ?>
-                        <a href="<?php echo esc_url( home_url('/') ); ?>">
-                            <img src=<?php echo get_template_directory_uri() . '/assets/img/logo.png';?> alt="Logo Paulo Vitor Designer">
-                        </a>
-                    <?php endif; ?>
+                <div class="d-flex justify-content-between container">
+
+                    <div class="d-flex align-items-center">
+                        <div class="logo">
+                            <?php if(has_custom_logo() ): ?>
+                                <?php the_custom_logo(); ?>
+                            <?php else: ?>
+                                <a href="<?php echo esc_url( home_url('/') ); ?>">
+                                    <img src=<?php echo get_template_directory_uri() . '/assets/img/logo.png';?> alt="Logo Paulo Vitor Designer">
+                                </a>
+                            <?php endif; ?>
+                        </div>
+                        <div class="d-none d-lg-block">
+                            <?php wp_nav_menu(
+                                array(
+                                    'theme_location' => 'primary_menu',
+                                    'depth' => 1
+                                    )
+                                ); 
+                            ?>
+                        </div>
                     </div>
-                    <div>
-                        <?php wp_nav_menu(
-                            array(
-                                'theme_location' => 'primary_menu',
-                                'depth' => 1
-                                )
-                            ); 
-                        ?>
+
+                    <div class="d-flex align-items-center">
+                        <div class="nav-bnt d-none d-none d-lg-block">
+                            <a href="">Orçamento</a>
+                        </div>
+                        <div class="bnt-icons d-flex">
+                            <ul class="d-flex nav-icon mb-0">
+                                <li class="d-none d-lg-block">
+                                    <a href=""><img src=<?php echo get_template_directory_uri() . '/assets/img/instagram.png';?> alt="Instagram"></a>
+                                </li>
+                                <li class="d-none d-lg-block">
+                                    <a href=""><img src=<?php echo get_template_directory_uri() . '/assets/img/facebook.png';?> alt="Facebook"></a>
+                                </li>
+                                <li class="d-none d-lg-block">
+                                    <a href=""><img src=<?php echo get_template_directory_uri() . '/assets/img/Drible.png';?> alt="Drible"></a>
+                                </li>
+                                <li class="d-none d-lg-block">
+                                    <a href=""><img src=<?php echo get_template_directory_uri() . '/assets/img/Pinterest.png';?> alt="Pinterest"></a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="text-white d-block d-lg-none">
+                            oi
+                        </div>
                     </div>
+
                 </div>
             </nav>
-            <div>
-                <img class="w-100" src=<?php echo get_template_directory_uri() . '/assets/img/banner.png';?> alt="Logo Paulo Vitor Designer">
+        </div>
+        <div class="nav-banner">
+            <img class="w-100" src=<?php echo get_template_directory_uri() . '/assets/img/banner.png';?> alt="Logo Paulo Vitor Designer">
+            <div class="container">
+                <h1>
+                    A ideia da marca vale mais
+                    <br>
+                    que a própria marca.
+                </h1>
+                <a class="nav-banner-bnt" href="">Veja mais</a>
             </div>
+        </div>
         </section>
     </header>
