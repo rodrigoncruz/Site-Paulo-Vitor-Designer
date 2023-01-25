23a3ebd6955950333da4fb47fcc6aab00c1383ed
Rodrigo Cruz <rodrigonascimentocruz@gmail.com>
2023-01-25 16:22:22 -0300

    finalização página portifólio - desktop

--- page-portfolio.php
+++ page-portfolio.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
   <main class="portifolio">
-    <div class="nav-banner">
+    <div class="nav-banner-portfolio">
         <img class="img-banner position-relative" src=<?php the_field('banner_portfolio'); ?> alt="Banner Portfólio">
         <div class="container">
             <div  class="position-absolute text-banner">
@@ -22,29 +22,43 @@ get_header(); ?>
     </div>
 
     <section class="row">
-      <div class="col-md-6 col-12"  id="sobre">
-        <img class="img-paulo" src=<?php the_field('foto');?> alt="Foto do Designer Paulo Vitor">
-      </div>
-      <div class="col-md-6 col-12 bg-blue">
-        <div class="about">
-          <h2 class="title "><?php the_field('titulo_paulo');?></h2>
-          <p><?php the_field('apresentacao_paragrafo_01');?></p>
-          <p><?php the_field('apresentacao_paragrafo_02');?></p>
-          <a href=<?php the_field('link_botao_paulo_vitor'); ?>><?php the_field('texto_botao');?></a>
-        </div>
-      </div>
-    </section>
+      <?php $count = 0 ?>
+    <?php if (have_rows('empresas')): while (have_rows('empresas')) : the_row();
+            $count++;
+            $imagem = get_sub_field('imagem');
+            $titulo = get_sub_field('titulo_empresa');
+            $descricao = get_sub_field('descricao_empresa');  
+            $alt = get_sub_field('alt_empresa_portifolio');
+            $link = get_sub_field('link_botao_empresas');
+            $texto_bnt = get_sub_field('texto_botao_empresas'); ?>
 
-      <div id="portfolio">
-        <section class="row overlay">
-          <?php if (have_rows('logos')): while (have_rows('logos')) : the_row();
-              $imagem = get_sub_field('imagem_empresas');
-              $link = get_sub_field('link_empresas'); 
-              $alt = get_sub_field('alt_empresas'); ?>
-                <div class="col-md-4"><a href=<?php  echo $link ?>><img class="img-paulo" src= <?php  echo $imagem ?> alt=<?php $alt ?>></a></div>
-          <?php endwhile; endif; ?>
-        </section>
-      </div>
+          <?php if ($count % 2 == 1 ) : ?>
+          <div class="col-md-6 col-12">
+            <img class="img" src=<?php echo $imagem ?> alt="<?php echo $alt ?>">
+          </div>
+          <div class="col-md-6 col-12">
+            <div class="about">
+              <h2 class="title "><?php echo $titulo ?></h2>
+              <p><?php echo $descricao ?></p>
+              <a href=<?php echo $link ?>><img class="mt-0 seta-empresas" src=<?php echo get_template_directory_uri() . '/assets/img/seta_empresas_es.svg' ?>><?php echo $texto_bnt ?></a>
+            </div>
+          </div>
+
+          <?php else : ?>
+            
+            <div class="col-md-6 col-12">
+              <div class="about-par">
+                <h2 class="title "><?php echo $titulo ?></h2>
+                <p><?php echo $descricao ?></p>
+                <a href=<?php echo $link ?>><?php echo $texto_bnt ?><img class="mt-0 seta-oposta" src=<?php echo get_template_directory_uri() . '/assets/img/seta-portifolio.svg' ?>></a>
+              </div>
+            </div>
+          <div class="col-md-6 col-12">
+            <img class="img" src=<?php echo $imagem ?> alt="<?php echo $alt ?>">
+          </div>
+          <?php endif; ?>
+      <?php endwhile; endif; ?>
+    </section>
   </main>
 <?php get_footer(); ?>
   
