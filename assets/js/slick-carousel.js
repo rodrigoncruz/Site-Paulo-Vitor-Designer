cce1edc449ad4fb85604dab60ecf747703fcd4cd
Rodrigo <rodrigonascimentocruz>
2023-04-01 14:12:08 -0300

    ajustes

--- assets/js/slick-carousel.js
+++ assets/js/slick-carousel.js
@@ -65,22 +65,26 @@ jQuery(document).ready(function($) {
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
-          infinite: true,
-          dots: true
+          dots: false,
+          arrows: false
         }
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 2,
+          dots: false,
+          arrows: false
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          dots: false,
+          arrows: false
         }
       }
       // You can unslick at a given breakpoint now by adding:
